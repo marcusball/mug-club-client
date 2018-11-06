9be886cc78bc4b252ecbf072d34f44f171f8b2ad
Marcus Ball <marcus.ball@live.com>
2018-11-05 23:56:08 -0500

    remove Vue default home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home container">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to The Midnight Mug Club"/>
   </div>
 </template>
 
