9394172a4a71099b1ca025882360ce5aa373bae2
Marcus Ball <marcus.ball@live.com>
2018-12-03 21:11:03 -0500

    Actually fill out the "about" page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>This is your virtual Mug Club drink sheet! Use this to keep track of your Mug Club progress without needing pull out your sheet after every drink.</p>
+
+    <p>
+      Note: This is
+      <em>not</em> an official Mug Club sheet substitute. You will still need to eventually update your physical paper sheet with the progress you log here.
+    </p>
+
+    <p>This is still being updated and improved. Any suggestions or comments to help improve this are greatly appreciated.</p>
+
+    <p>
+      <router-link
+        v-if="!isLoggedIn"
+        to="/auth"
+        class="btn btn-primary"
+      >Create an account to get started</router-link>
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    isLoggedIn: function() {
+      return this.$store.getters.isLoggedIn;
+    }
+  }
+};
+</script>
